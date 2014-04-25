419b2d2db002f35b8e74a010013cbb945d72667f
cigaly <cedomir.igaly@e-dama.net>
2014-04-25 19:50:02 +0200

    Killed!!

--- getFeatures.R
+++
@@ -1,7 +0,0 @@
-getFeatures <- function(fileName) {
-	features <- read.csv(fileName, sep=' ', header=FALSE)
-	columns<-grep("std\\(\\)|mean\\(\\)", features[[2]])
-	result <- features[columns,]
-	names(result) <- c("index", "title")
-        return(result)
-}
